dfb6cc26b0571f9ea62f4615bc195be31e4f4aec
eserin74 <ekremserincv@gmail.com>
2024-08-01 16:37:35 +0300

    Create common-be

---
+++ main/java/com/easydev/common/CommonBeApplication.java
@@ -0,0 +1,13 @@
+package com.easydev.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonBeApplication.class, args);
+    }
+
+}
---
+++ test/java/com/easydev/common/CommonBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easydev.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
